40c473d50c6b3c9e10b27f954953c205bd324b37
nn <charlotte.hc.com@gmail.com>
2025-02-18 22:40:16 +0400

    добавил доп функции; вызывает исключения, т.к. пытается прочесть список с выделенной памятью, но не заполненный

--- Task/Task.cpp
+++ Task/Task.cpp
@@ -5,19 +5,60 @@
 #include "Task.h"
 #include <iostream>
 
+void AddFirst(ListItem* newList, ListItem* firstItem)
+{
+	ListItem* temp = new ListItem;
+	temp->id = firstItem->id;
+	strcpy_s(temp->name, firstItem->name);
+	temp->price = firstItem->price;
+	temp->count = firstItem->count;
+	newList->next = temp;
+}
+
+ListItem* GetLast(ListItem* newList)
+{
+	if (newList == nullptr) return nullptr; // если список пуст, то вернем пустой указатель
+	auto temp = newList; // временной переменной присваиваем указатель на первый элемент
+	while (temp->next != nullptr) // пока ее поле next не пустой указатель (если пустой, то мы достигли последнего элемента!)
+	{
+		// запомните это присваивание - это переход к следующему в списке элементу
+		temp = temp->next;  // перемещаемся к следующему элементу списка
+	}
+	return temp; // возвращаем указатель на последний элемент
+}
+
+void AddLast(ListItem* newList, ListItem* firstItem)
+{
+	if (firstItem == nullptr) // если список пуст, вызовем функцию добавления в начало списка
+		AddFirst(newList, firstItem);
+	ListItem* temp = new ListItem; // создаем в памяти новый элемент списка
+	temp->id = firstItem->id;   // присваиваем полям элемента нужные значения
+	strcpy_s(temp->name, firstItem->name);
+	temp->price = firstItem->price;
+	temp->count = firstItem->count;
+	temp->next = nullptr; // нам нужно встроить элемент в конец списка, поэтому
+	// указатель next настраиваем таким образом, чтобы он был пустым
+	// который ранее был первым (на него указывает first)
+	GetLast(newList)->next = temp;    // а теперь делаем, чтобы элемент, который до этого был последним
+	// (а его мы получаем с помощью уже созданной GetLast)
+	// ссылался на наш новый элемент
+}
+
+
 ListItem* SearchByPriceRange(ListItem* firstItem, float low, float high)
 {
 	if (firstItem == nullptr) return nullptr;
-	ListItem* temp = new ListItem;
-	while (firstItem->next != nullptr)
+	ListItem* newList = new ListItem;
+	while (firstItem != nullptr)
 	{
 		if (firstItem->price <= high && firstItem->price >= low)
 		{
-			temp->id = firstItem->id;
-			strcpy_s(temp->name, firstItem->name);
-			temp->price = firstItem->price;
+			//AddFirst(newList, firstItem);
+			auto lastItem = GetLast(newList);
+			AddLast(newList, firstItem);
 		}
 	}
+	return newList;
 }
 
 /*
